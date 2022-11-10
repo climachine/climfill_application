cc027e21b879d2ae2a444c514bb9149d5035bb96
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2022-11-10 16:25:59 +0100

    read initguess, kwargs for DTR, deep copy, clean up post-interp gap-fill

--- interpolation.py
+++ interpolation.py
@@ -15,17 +15,16 @@ testcase = args.testcase
 
 esapath = '/net/so4/landclim/bverena/large_files/climfill_esa/'
 
-# read data
+# read data, NEW: without crossval holes
 print(f'{datetime.now()} read data...')
-data = xr.open_dataset(f'{esapath}{testcase}/data_crossval.nc')
+data = xr.open_dataset(f'{esapath}{testcase}/data_initguess.nc')
 landmask = xr.open_dataset(f'{esapath}landmask.nc').landmask
 
 # xarray/dask issue https://github.com/pydata/xarray/issues/3813
 # value assignment only works if non-dask array
 data = data.to_array().load()
 
-# divide into monthly climatology and anomalies
-print(f'{datetime.now()} divide clim and anomalies...')
+# divide into climatology and anomalies
 data_monthly = data.groupby('time.month').mean()
 data_anom = data.groupby('time.month') - data_monthly 
 
@@ -54,8 +53,11 @@ rbf_kwargs = {'burned_area':              {'neighbors': 100,
                                            'degree': 1},
               'terrestrial_water_storage':{'neighbors': 100, 
                                            'smoothing': 0.1, 
+                                           'degree': 1},
+              'diurnal_temperature_range':{'neighbors': 100, 
+                                           'smoothing': 0.1, 
                                            'degree': 1}}
-data_monthly = gapfill_thin_plate_spline(data_monthly, landmask, rbf_kwargs)
+data_monthly = gapfill_thin_plate_spline(data_monthly.copy(deep=True), landmask, rbf_kwargs)
 
 # gapfill anomalies with kriging
 print(f'{datetime.now()} gapfill anomalies...')
@@ -90,22 +92,28 @@ kriging_kwargs = {'burned_area':              {'constant_value': 100,
                   'terrestrial_water_storage':{'constant_value': 100, 
                                                'length_scale': 10, 
                                                'npoints': 100, 
+                                               'repeats': 5},
+                  'diurnal_temperature_range':{'constant_value': 100, 
+                                               'length_scale': 10, 
+                                               'npoints': 100, 
                                                'repeats': 5}}
 import warnings # DEBUG
 warnings.simplefilter('ignore')
-data_anom = gapfill_kriging(data_anom, landmask, kriging_kwargs)
+data_anom = gapfill_kriging(data_anom.copy(deep=True), landmask, kriging_kwargs)
 
 # step 1.4: add monthly climatology and anomalies back together
+# implicitly here: time series elongation with seasonal cycle
 print(f'{datetime.now()} add together...')
 data = data_anom.groupby('time.month') + data_monthly
 data = data.drop('month') # month not needed anymore
 
-# necessary if full days are missing: fill all remaining gaps with variable mean
-# TODO interpolate in time, because mostly full slices are missing
-print(f'{datetime.now()} last missing values...')
+# necessary if full months are missing: interpolate in time
+print(f'{datetime.now()} fill individual months with time interp...')
 if np.isnan(data).sum() != 0: # if still missing values present
-    print('still missing values treatment')
     data = data.interpolate_na(dim='time', method='linear')
+
+if np.isnan(data).sum() != 0: # if still missing values present
+    print(f'{datetime.now()} last gaps fill with variable mean ...')
     variable_mean = data.mean(dim=('time', 'lat', 'lon'))
     data = data.fillna(variable_mean)
 
