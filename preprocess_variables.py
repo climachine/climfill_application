deac0f487add0bfc191cb1811172a10aad251de6
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2022-11-10 16:27:07 +0100

    add permafrost, add landcover, snow yearly, start swnet

--- preprocess_variables.py
+++ preprocess_variables.py
@@ -50,18 +50,21 @@ ds_out = xr.Dataset({'lat': (['lat'], np.arange(-89.75,90, 0.5)), # same as cdo_
 
 # flags for running scripts
 ifire = False
+ipermafrost = False
+ilandcover = False
 ilstpre = False
 ilstpost = False
 ilstpostinit = False
 idtr = False
 idtrinit = False
-isnow = False
+isnowpre = False
+isnowpost = True
 ism = False
 isminit = False
 itws = False
 iprecip = False
 iobs = False
-iobsinit = True
+iobsinit = False
 inetrad = False
 
 # fire
@@ -79,14 +82,34 @@ if ifire:
     data = data.to_dataset(name='burned_area')
     data.to_netcdf(f'{esapath}burned_area.nc')
 
+# permafrost
+if ipermafrost:
+    filepath = landclimstoragepath + \
+               'ESA-CCI-Permafrost/v3.0/1km_polar-stereographic_1y/processed/regrid0.5deg/'
+    data = xr.open_mfdataset(f'{filepath}*.nc')['PFR']
+
+    data = data.sel(time=timeslice)
+
+    # assumption: since "perma", frozen whole year
+    data = data.resample(time='MS').ffill()
+
+    data = data.to_dataset(name='permafrost')
+    data.to_netcdf(f'{esapath}permafrost.nc')
+
 # land cover
-#if ilandcover:
-#    filepath = landclimstoragepath + \
-#               'ESA-CCI-LC_Land-Cover-Maps/v2.0.7cds/300m_plate-carree_1y/original/'
-#    data = xr.open_mfdataset(f'{filepath}*.nc')['lccs_class']
-#regridder = xe.Regridder(data, ds_out, 'bilinear', reuse_weights=False) #gives TypeError: buffer is too small for requested array
-#data = regridder(data)
-#cdo remaplaf,r360x180 /net/exo/landclim/data/dataset/ESA-CCI-LC_Land-Cover-Maps/v2.0.7cds/300m_plate-carree_1y/original/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2003-v2.0.7cds.nc test.nc # remaplaf is for land cover classes; fulden says consider aggregating the classes for more useful results
+if ilandcover:
+    filepath = landclimstoragepath + \
+               'ESA-CCI-LC_Land-Cover-Maps/v2.0.7cds/300m_plate-carree_1y/processed/regrid0.5deg/'
+    data = xr.open_mfdataset(f'{filepath}*.nc')['lccs_class']
+
+    # regridding not necesssary, already done by MH
+
+    data = data.sel(time=timeslice)
+
+    data = data.resample(time='MS').ffill()
+
+    data = data.to_dataset(name='landcover')
+    data.to_netcdf(f'{esapath}landcover.nc')
 
 # land surface temperature
 # missing values: cloud cover, swaths
@@ -197,33 +220,89 @@ if idtrinit:
     data.to_netcdf(f'{esapath}diurnal_temperature_range_init.nc')
 
 # snow water equivalent
-if isnow:
-    snowpath = landclimstoragepath + 'ESA-CCI-Snow_SWE/v2.0/0.1deg_lat-lon_1d/original/'
-    data = xr.open_mfdataset(f'{snowpath}*.nc')['swe']
-     
-    # all negative values are masks (ocean, mountain, glacier) set to nan
-    mask = data >= 0 # keep those values
+#if isnow:
+#    snowpath = landclimstoragepath + 'ESA-CCI-Snow_SWE/v2.0/0.1deg_lat-lon_1d/original/'
+#    data = xr.open_mfdataset(f'{snowpath}*.nc')['swe']
+#     
+#    # all negative values are masks (ocean, mountain, glacier) set to nan
+#    mask = data >= 0 # keep those values
+#
+#    # resample to month: checked that all months have 21 valid days everywhere (bec only constant missingness)
+#    data = data.resample(time='MS').mean()
+#    mask = mask.resample(time='MS').mean()
+#    data = data.sel(time=timeslice)
+#
+#    # set missing values (bec all in summer from missing files, checked)
+#    # to 0 because they imply NO snow
+#    data = data.fillna(0)
+#
+#    # insert missing values from mountains and glaciers again
+#    data = data.where(mask)
+#
+#    # set SH values to 0, not NAN, because we assume no snow in SH
+#    data = data.where(data.lat > 0, 0)
+#
+#    regridder = xe.Regridder(data, ds_out, 'bilinear', reuse_weights=False) 
+#    data = regridder(data)
+#
+#    data = data.to_dataset(name='snow_water_equivalent')
+#    data.to_netcdf(f'{esapath}snow_water_equivalent.nc')
 
-    # resample to month: checked that all months have 21 valid days everywhere (bec only constant missingness)
-    data = data.resample(time='MS').mean()
-    mask = mask.resample(time='MS').mean()
-    data = data.sel(time=timeslice)
+#if isnowpre:
+#    import glob
+#    import os
+#    filepath = landclimstoragepath + \
+#               'ESA-CCI-Snow_SCFG/v2.0/5km_lat-lon_1d/original/AVHRR/'
+#    filenames = glob.glob(f'{filepath}*.nc')
+#    if not os.path.exists('cdo_weights_snow.nc'): 
+#        os.system(f'cdo genbil,cdo_grid_description.txt {filenames[0]} cdo_weights_snow.nc')
+#
+#    # regrid all files individually
+#    # Remap weights not used because missing values are different for each timestep
+#    # https://code.mpimet.mpg.de/projects/cdo/wiki/FAQ (search "remap weights")
+#    # although error "weights not used" is faster with weights
+#    for filename in filenames:
+#        savename = f'{esapath}snow_daily/snow_{filename.split("/")[-1][:8]}.nc'
+#        if not os.path.exists(f'{savename}'):
+#            cmd = f'cdo -P 20 remap,r720x360,cdo_weights_snow.nc -selname,scfg {filename} {savename} \n'
+#            with open('run_snow_regrid_cdo.sh','a') as file:
+#                file.write(cmd)
 
-    # set missing values (bec all in summer from missing files, checked)
-    # to 0 because they imply NO snow
-    data = data.fillna(0)
+# snow cover fraction
+if isnowpre: 
+    filepath = landclimstoragepath + \
+               'ESA-CCI-Snow_SCFG/v2.0/5km_lat-lon_1d/original/AVHRR/'
+    years = np.arange(1995,2021)
+    
+    for year in years:
+        print(year)
+        try:
+            data = xr.open_mfdataset(f'{filepath}/{year}*.nc')['scfg']
+        except OSError as e:
+            print(e)
+            continue
 
-    # insert missing values from mountains and glaciers again
-    data = data.where(mask)
+        # missing values are above 200
+        data = data.where(data <= 100, np.nan)
 
-    # set SH values to 0, not NAN, because we assume no snow in SH
-    data = data.where(data.lat > 0, 0)
+        # create mask for less than 15 days of data
+        mask = np.isnan(data).astype(float).resample(time='MS').sum()
+        mask = mask <= 15
+        data = data.resample(time='MS').mean()
+        data = data.where(mask)
 
-    regridder = xe.Regridder(data, ds_out, 'bilinear', reuse_weights=False) 
-    data = regridder(data)
+        # regrid
+        regridder = xe.Regridder(data, ds_out, 'bilinear', reuse_weights=False) 
+        data = regridder(data)
 
-    data = data.to_dataset(name='snow_water_equivalent')
-    data.to_netcdf(f'{esapath}snow_water_equivalent.nc')
+        # save
+        data = data.to_dataset(name='snow_cover_fraction')
+        data.to_netcdf(f'{esapath}snow_yearly/snow_cover_fraction_{year}.nc')
+
+if isnowpost:
+    data = xr.open_mfdataset(f'{esapath}snow_yearly/*.nc')
+    import IPython; IPython.embed()
+    data.to_netcdf(f'{esapath}snow_cover_fraction.nc')
 
 # soil moisture:
 # missing values: below dense vegetation, swaths(?)
@@ -369,4 +448,7 @@ if iobsinit:
     data.to_netcdf(f'{esapath}precipitation_obs_init.nc')
 
 if inetrad:
-    pass
+    filepath = landclimstoragepath + \
+               'ESA-CCI-Cloud_AVHRR-PM/v3.0/0.5deg_lat-lon_1m/original/'
+    data = xr.open_mfdataset(f'{filepath}*.nc')
+    import IPython; IPython.embed()
