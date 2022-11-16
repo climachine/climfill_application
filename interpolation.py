5bc8e4bfe4d2c9872d1494a4b9e4aa80e7fc7d5b
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2022-11-16 09:19:54 +0100

    from crossval data, cv rbfkwargs, add landcover, gapfill climatology

--- interpolation.py
+++ interpolation.py
@@ -5,7 +5,7 @@ interpolate initial gapfill
 import numpy as np
 import xarray as xr
 import argparse
-from datetime import datetime
+from datetime import datetime, timedelta
 from climfill.interpolation import gapfill_thin_plate_spline, gapfill_kriging
 
 parser = argparse.ArgumentParser()
@@ -17,7 +17,7 @@ esapath = '/net/so4/landclim/bverena/large_files/climfill_esa/'
 
 # read data, NEW: without crossval holes
 print(f'{datetime.now()} read data...')
-data = xr.open_dataset(f'{esapath}{testcase}/data_initguess.nc')
+data = xr.open_dataset(f'{esapath}{testcase}/data_crossval.nc')
 landmask = xr.open_dataset(f'{esapath}landmask.nc').landmask
 
 # xarray/dask issue https://github.com/pydata/xarray/issues/3813
@@ -31,31 +31,34 @@ data_anom = data.groupby('time.month') - data_monthly
 # gapfill monthly data with thin-plate-spline interpolation
 print(f'{datetime.now()} gapfill seasonal...')
 rbf_kwargs = {'burned_area':              {'neighbors': 100, 
-                                           'smoothing': 0.1, 
+                                           'smoothing': 100, 
                                            'degree': 1},
               'soil_moisture':            {'neighbors': 100,
-                                           'smoothing': 10, 
-                                           'degree': 2}, 
+                                           'smoothing': 100, 
+                                           'degree': 1}, 
               'precipitation_obs':        {'neighbors': 100, 
                                            'smoothing': 0.1, 
                                            'degree': 1},
               'precipitation':            {'neighbors': 100, 
+                                           'smoothing': 100, 
+                                           'degree': 1},
+              'snow_cover_fraction':      {'neighbors': 100, 
                                            'smoothing': 0.1, 
                                            'degree': 1},
-              'snow_water_equivalent':    {'neighbors': 100, 
+              'landcover':                {'neighbors': 100, 
                                            'smoothing': 0.1, 
                                            'degree': 1},
               'surface_temperature':      {'neighbors': 100, 
-                                           'smoothing': 0.1, 
+                                           'smoothing': 100, 
                                            'degree': 1},
               'temperature_obs':          {'neighbors': 100, 
-                                           'smoothing': 0.1, 
+                                           'smoothing': 100, 
                                            'degree': 1},
               'terrestrial_water_storage':{'neighbors': 100, 
-                                           'smoothing': 0.1, 
+                                           'smoothing': 1, 
                                            'degree': 1},
               'diurnal_temperature_range':{'neighbors': 100, 
-                                           'smoothing': 0.1, 
+                                           'smoothing': 10, 
                                            'degree': 1}}
 data_monthly = gapfill_thin_plate_spline(data_monthly.copy(deep=True), landmask, rbf_kwargs)
 
@@ -77,7 +80,11 @@ kriging_kwargs = {'burned_area':              {'constant_value': 100,
                                                'length_scale': 10, 
                                                'npoints': 100, 
                                                'repeats': 5},
-                  'snow_water_equivalent':    {'constant_value': 100, 
+                  'snow_cover_fraction':      {'constant_value': 100, 
+                                               'length_scale': 10, 
+                                               'npoints': 100, 
+                                               'repeats': 5},
+                  'landcover':                {'constant_value': 100, 
                                                'length_scale': 10, 
                                                'npoints': 100, 
                                                'repeats': 5},
@@ -102,15 +109,23 @@ warnings.simplefilter('ignore')
 data_anom = gapfill_kriging(data_anom.copy(deep=True), landmask, kriging_kwargs)
 
 # step 1.4: add monthly climatology and anomalies back together
-# implicitly here: time series elongation with seasonal cycle
 print(f'{datetime.now()} add together...')
 data = data_anom.groupby('time.month') + data_monthly
 data = data.drop('month') # month not needed anymore
 
 # necessary if full months are missing: interpolate in time
-print(f'{datetime.now()} fill individual months with time interp...')
+#print(f'{datetime.now()} fill individual months with time interp...')
+#import IPython; IPython.embed()
+#if np.isnan(data).sum() != 0: # if still missing values present
+#    pass
+#    #data = data.interpolate_na(dim='time', method='linear', max_gap=timedelta(days=32)) # Module not found bottleneck
+
+# elongate time series: gapfill with existing climatology
 if np.isnan(data).sum() != 0: # if still missing values present
-    data = data.interpolate_na(dim='time', method='linear')
+    years = np.unique(data['time.year'])
+    for year in years:
+        tslice=slice(f'{year}-01-01',f'{year}-12-31')
+        data.loc[:,tslice,:,:] = data.loc[:,tslice,:,:].fillna(data_monthly.values)
 
 if np.isnan(data).sum() != 0: # if still missing values present
     print(f'{datetime.now()} last gaps fill with variable mean ...')
