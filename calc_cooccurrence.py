a9b4bc62268ff93f105983662e4a0cf2420a2595
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2022-12-23 09:30:40 +0100

    ordering of vars like in plots

--- calc_cooccurrence.py
+++ calc_cooccurrence.py
@@ -9,9 +9,8 @@ import xarray as xr
 
 esapath = '/net/so4/landclim/bverena/large_files/climfill_esa/'
 varnames = ['soil_moisture','surface_temperature','precipitation',
-            'terrestrial_water_storage','snow_cover_fraction',
-            'temperature_obs','precipitation_obs', 
-            'diurnal_temperature_range','burned_area'] 
+            'terrestrial_water_storage','temperature_obs','precipitation_obs',
+            'snow_cover_fraction','diurnal_temperature_range','burned_area'] 
 
 # read data
 mask = xr.open_dataset(f'{esapath}mask_orig.nc') 
@@ -30,5 +29,4 @@ for (var1, var2) in variable_combinations:
     both_obs = (np.logical_not(mask[var1]) & np.logical_not(mask[var2])).sum().item()
     frac_obs = np.around((both_obs / n_relevant.item())*100, decimals=2)
     df.loc[var1,var2] = frac_obs
-import IPython; IPython.embed()
-        
+df.to_csv('frac_obs_twovars.csv') 
