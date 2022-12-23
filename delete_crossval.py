de9cbddd9fc4a589304eab9748b2ba8b97c04822
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2022-12-23 09:32:31 +0100

    create minicubes and save separately, not deleteing in main file anymore

--- delete_crossval.py
+++ delete_crossval.py
@@ -8,7 +8,8 @@ import argparse
 import numpy as np
 import xarray as xr
 
-from climfill.verification import delete_minicubes
+#from climfill.verification import delete_minicubes
+from climfill.verification import create_minicubes
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--testcase', '-t', dest='testcase', type=str)
@@ -37,25 +38,58 @@ mask = mask.where(landmask, np.nan) # is needed for calc of n_obs
 varnames = list(data.coords['variable'].values)
 varnames.remove('landcover')
 
+# select year
+mask = mask.sel(time=slice(vf_year,cv_year)).load()
+data = data.sel(time=slice(vf_year,cv_year)).load()
+
+# copy 10 times
+datasets = []
+for i in range(10):
+    datasets.append(data.copy(deep=True))
+
+# create minicubes (on land)
+minicubes = create_minicubes(mask.sel(variable='soil_moisture').drop('variable'),
+                             ncubes)
+
+# divide cubes on land into ten roughly equally sized sets
+cubes_on_land = np.unique(minicubes)
+cubes_on_land = cubes_on_land[~np.isnan(cubes_on_land)]
+
+for varname in varnames:
+
+    np.random.shuffle(cubes_on_land)
+    random_sets = np.array_split(cubes_on_land, 10)
+
+    for dat, cubeset in zip(datasets, random_sets):
+
+        dat.loc[varname,:,:,:] = dat.loc[varname,:,:,:].where(np.logical_not(minicubes.isin(cubeset)))
+        print((np.isnan(dat).sum(dim=('lat','lon','time')) / np.isnan(dat).count(dim=('lat','lon','time'))).values)
+
+# save
+for d, dat in enumerate(datasets):
+    mask = np.isnan(dat)
+    mask.to_dataset('variable').to_netcdf(f'{esapath}{testcase}/verification/maskveri{d}.nc')
+    dat.to_dataset('variable').to_netcdf(f'{esapath}{testcase}/verification/dataveri{d}.nc')
+
 # delete values for verification
-for varname in varnames:
-    print(f'{datetime.now()} verification {varname}...')
-    tmp = delete_minicubes(mask.sel(time=vf_year, variable=varname).drop('variable').load(),
-                           frac_mis, ncubes)
-    mask.loc[dict(variable=varname, time=vf_year)] = tmp
-
-data = data.where(np.logical_not(mask))
-
-# delete values for cross-validation
-for varname in varnames:
-    print(f'{datetime.now()} cross-validation {varname}...')
-    tmp = delete_minicubes(mask.sel(time=cv_year, variable=varname).drop('variable').load(),
-                           frac_mis, ncubes)
-    mask.loc[dict(variable=varname, time=cv_year)] = tmp
-
-data = data.where(np.logical_not(mask))
+#for varname in varnames:
+#    print(f'{datetime.now()} verification {varname}...')
+#    tmp = delete_minicubes(mask.sel(time=vf_year, variable=varname).drop('variable').load(),
+#                           frac_mis, ncubes)
+#    mask.loc[dict(variable=varname, time=vf_year)] = tmp
+#
+#data = data.where(np.logical_not(mask))
+#
+## delete values for cross-validation
+#for varname in varnames:
+#    print(f'{datetime.now()} cross-validation {varname}...')
+#    tmp = delete_minicubes(mask.sel(time=cv_year, variable=varname).drop('variable').load(),
+#                           frac_mis, ncubes)
+#    mask.loc[dict(variable=varname, time=cv_year)] = tmp
+#
+#data = data.where(np.logical_not(mask))
 
 # save crossval cube for gap-filling
-print(f'{datetime.now()} save...')
-data.to_dataset('variable').to_netcdf(f'{esapath}{testcase}/data_crossval.nc')
-mask.to_dataset('variable').to_netcdf(f'{esapath}{testcase}/mask_crossval.nc')
+#print(f'{datetime.now()} save...')
+#data.to_dataset('variable').to_netcdf(f'{esapath}{testcase}/data_crossval.nc')
+#mask.to_dataset('variable').to_netcdf(f'{esapath}{testcase}/mask_crossval.nc')
