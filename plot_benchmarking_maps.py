29bb15db2ab6b8c8d7c8319c8eaae7a6549b1159
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2023-01-10 10:53:24 +0100

    new varnames, new order

--- plot_benchmarking_maps.py
+++ plot_benchmarking_maps.py
@@ -38,37 +38,19 @@ orig = xr.open_dataset(f'{esapath}data_orig.nc')
 fill = xr.open_dataset(f'{esapath}{testcase}/data_climfilled.nc')
 era5 = xr.open_dataset(f'{esapath}data_era5land.nc')
 
-#mask_orig = xr.open_dataset(f'{esapath}mask_orig.nc')
-#mask_cv = xr.open_dataset(f'{esapath}{testcase}/mask_crossval.nc')
-
 # (optional) calculate anomalies
 orig = orig.groupby('time.month') - orig.groupby('time.month').mean()
 era5 = era5.groupby('time.month') - era5.groupby('time.month').mean()
 fill = fill.groupby('time.month') - fill.groupby('time.month').mean()
 
-# select verification year
-#mask_orig = mask_orig.sel(time=verification_year).load()
-#mask_cv = mask_cv.sel(time=verification_year).load()
-#orig = orig.sel(time=verification_year).load()
-#intp = intp.sel(time=verification_year).load()
-#fill = fill.sel(time=verification_year).load()
-
-# calculate mask of verification points
-#mask_cv = np.logical_and(np.logical_not(mask_orig), mask_cv)
-
-# mask everything except verification points
-#orig = orig.where(mask_cv)
-#intp = intp.where(mask_cv)
-#fill = fill.where(mask_cv)
-
 # sort data
 varnames = ['soil_moisture','surface_temperature','precipitation',
-            'temperature_obs','precipitation_obs',
-            'diurnal_temperature_range','snow_cover_fraction'] 
-varnames_plot = ['surface layer \nsoil moisture','surface temperature',
-                 'precipitation (sat)','2m temperature',
-                 'precipitation (ground)',
-                 'diurnal temperature range sfc','snow cover fraction'] 
+            'temperature_obs','precipitation_obs','snow_cover_fraction',
+            'diurnal_temperature_range'] 
+#varnames_plot = ['surface layer \nsoil moisture','surface temperature',
+#                 'precipitation (sat)','2m temperature',
+#                 'precipitation (ground)',
+#                 'diurnal temperature range sfc','snow cover fraction'] 
 orig = orig.to_array().reindex(variable=varnames)
 era5 = era5.to_array().reindex(variable=varnames)
 fill = fill.to_array().reindex(variable=varnames)
@@ -78,6 +60,7 @@ fill = fill.to_array().reindex(variable=varnames)
 landmask = regionmask.defined_regions.natural_earth_v5_0_0.land_110.mask(orig.lon, orig.lat)
 regions = regionmask.defined_regions.ar6.land.mask(orig.lon, orig.lat)
 regions = regions.where(~np.isnan(landmask))
+obsmask = xr.open_dataset(f'{esapath}landmask.nc').landmask
 
 orig = orig.groupby(regions).mean()
 era5 = era5.groupby(regions).mean()
@@ -96,22 +79,52 @@ ax6 = fig.add_subplot(336, projection=proj)
 ax7 = fig.add_subplot(337, projection=proj)
 ax8 = fig.add_subplot(338, projection=proj)
 ax9 = fig.add_subplot(339, projection=proj)
-axes = [ax1,ax2,ax3,ax4,ax5,ax6,ax7,ax8,ax9]
-levels = np.arange(-1,1.25,0.25)
+levels = np.arange(-1,1.1,0.1)
 
+cmap = plt.get_cmap('seismic_r')
+cmap.set_over('aliceblue')
+cmap.set_bad('lightgrey')
+
+#varnames_plot = ['surface layer \nsoil moisture','surface temperature',
+#                 'precipitation (sat)','terrestrial water storage','2m temperature',
+#                 'precipitation (ground)', 'burned area',
+#                 'diurnal temperature range sfc','snow cover fraction'] 
+varnames_plot = ['SM','LST','PSAT','TWS','T2M','P2M','SCF','DTR','BA']
+
+#varnames = ['soil_moisture','surface_temperature','precipitation',
+#            'terrestrial_water_storage','temperature_obs','precipitation_obs',
+#            'burned_area','diurnal_temperature_range','snow_cover_fraction'] 
+axes = [ax1,ax2,ax3,ax5,ax6,ax7,ax8]
 for v, (varname, ax) in enumerate(zip(varnames, axes)):
     corrorig = xr.corr(orig.sel(variable=varname),era5.sel(variable=varname), dim='time')
     corrfill = xr.corr(fill.sel(variable=varname),era5.sel(variable=varname), dim='time')
 
+    # mask regions with less than 10% values from landmask (greenland etc)
+    count_landpoints = landmask.where(landmask!=0,1).groupby(regions).count()
+    count_obspoints = obsmask.groupby(regions).sum()
+    included_regions = count_obspoints/count_landpoints > 0.10
+
+    corrfill = corrfill.where(included_regions)
+    corrorig = corrorig.where(included_regions)
+
+    # back to worldmap
     corrorig = expand_to_worldmap(corrorig,regions)
     corrfill = expand_to_worldmap(corrfill,regions)
 
-    landmask.plot(ax=ax, add_colorbar=False, cmap='Greys', transform=transf, vmin=-2, vmax=10)
-    im = corrfill.plot(ax=ax, cmap='coolwarm_r', vmin=-1, vmax=1, transform=transf, 
+    # ocean blue
+    corrfill = corrfill.where(np.logical_not(landmask), 10) # ocean blue
+    corrorig = corrorig.where(np.logical_not(landmask), 10) # ocean blue
+
+    #landmask.plot(ax=ax, add_colorbar=False, cmap='Greys', transform=transf, vmin=-2, vmax=10)
+    im = corrfill.plot(ax=ax, cmap=cmap, vmin=-1, vmax=1, transform=transf, 
                                levels=levels, add_colorbar=False)
     regionmask.defined_regions.ar6.land.plot(line_kws=dict(color='black', linewidth=1), 
                                                  ax=ax, add_label=False, projection=transf)
-    ax.set_title(varnames_plot[v])
+
+axes = [ax1,ax2,ax3,ax4,ax5,ax6,ax7,ax8,ax9]
+for ax, varname in zip(axes, varnames_plot):
+    ax.set_title(varname)
+
 
 cbar_ax = fig.add_axes([0.93, 0.15, 0.02, 0.6]) # left bottom width height
 cbar = fig.colorbar(im, cax=cbar_ax, orientation='vertical')
