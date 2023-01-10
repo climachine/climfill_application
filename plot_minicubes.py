33756c0c59b54f8ce18105c625b32132a68f175c
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2023-01-10 10:54:23 +0100

    ocean blue, seismic colormap, legend

--- plot_minicubes.py
+++ plot_minicubes.py
@@ -27,6 +27,7 @@ varnames = ['soil_moisture','surface_temperature','precipitation',
 data = xr.open_dataset(f'{esapath}{testcase}/data_crossval.nc')
 mask_orig = xr.open_dataset(f'{esapath}mask_orig.nc')
 mask_cv = xr.open_dataset(f'{esapath}{testcase}/mask_crossval.nc')
+landmask = xr.open_dataset(f'{esapath}landmask.nc').landmask
 
 # calculate mask of verification points
 mask = np.logical_and(np.logical_not(mask_orig), mask_cv)
@@ -35,16 +36,21 @@ mask = np.logical_and(np.logical_not(mask_orig), mask_cv)
 data = data.soil_moisture.sel(time='2005-09-01')
 mask = mask.soil_moisture.sel(time='2005-09-01')
 
-# set ocean to negative
+# calculate ocean mask
 oceanmask = regionmask.defined_regions.natural_earth_v5_0_0.land_110.mask(data.lon,data.lat)
 oceanmask = ~np.isnan(oceanmask)
-data = data.where(oceanmask, -10)
-data = data.where(np.logical_not(mask), 10)
+
+# calculate mask of excluded areas 
+icedesert = np.logical_and(np.logical_not(landmask),oceanmask)
+
+data = data.where(oceanmask, -10) # ocean negative
+data = data.where(np.logical_not(mask), 10) # verification points set to 10
+#data = data.where(icedesert, np.nan) # ice and deserts nan 
 
 # plot
 proj = ccrs.Robinson()
 transf = ccrs.PlateCarree()
-cmap = plt.get_cmap('coolwarm_r')
+cmap = plt.get_cmap('seismic_r')
 cmap.set_under('aliceblue')
 cmap.set_over('grey')
 levels = np.arange(0,0.45,0.05)
@@ -54,10 +60,13 @@ fig = plt.figure(figsize=(10,5))
 ax = fig.add_subplot(111, projection=proj)
 data.plot(ax=ax, cmap=cmap, transform=transf, vmin=0, vmax=0.4, levels=levels,
           cbar_kwargs=cbar_kwargs)
+#icedesert.plot(ax=ax, hatch='\\', add_colorbar=False) #https://github.com/matplotlib/matplotlib/issues/3058
 ax.set_facecolor('lightgrey')
 ax.set_title('ESA CCI surface layer soil moisture, Sept 2005')
 legend_elements = [Patch(facecolor='grey', edgecolor='grey', 
-                   label='verification points')]
+                   label='verification points'),
+                   Patch(facecolor='lightgrey', edgecolor='lightgrey',
+                   label='unobserved points')]
 ax.legend(handles=legend_elements, loc='upper right',
           bbox_to_anchor=(1,0))
 plt.savefig('minicubes.png', dpi=300)
