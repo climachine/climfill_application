f2eba1a18e985fb1180865cc4c703d667c4c0134
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2023-01-18 13:54:49 +0100

    add initial guess to loop, clusters in sets also 30

--- run_verify.sh
+++ run_verify.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-TESTCASE='test7'
+TESTCASE='test8'
 N_CLUSTERS=10
 COUNT=$(expr $N_CLUSTERS - 1)
 
 for CLUSTER in $(seq 0 ${COUNT}); do
     #CLUSTER=$(expr $N_CLUSTERS - $CLUSTER)
     echo "set ${CLUSTER} calc ..."
+    python initial_guess.py -t ${TESTCASE} -s ${CLUSTER}
     python interpolation.py -t ${TESTCASE} -s ${CLUSTER}
     python feature_engineering.py -t ${TESTCASE} -s ${CLUSTER}
-    python clustering.py -t ${TESTCASE} -s ${CLUSTER} -c 8
+    python clustering.py -t ${TESTCASE} -s ${CLUSTER} -c 30
     #exit
 done
