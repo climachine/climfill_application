b2b066a146bf7b9934a1e78d4571b6b4d1c67860
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2023-01-18 13:55:19 +0100

    set backwards

--- run_regr_verify.sh
+++ run_regr_verify.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-TESTCASE='test7'
+TESTCASE='test8'
 N_SETS=10
-N_CLUSTER=8
-COUNT=$(expr $N_SETS - 1)
-N_CLUSTER=$(expr $N_CLUSTER - 1)
+N_CLUSTER=30
+COUNT=$(expr $N_SETS - 1) # pythonic counting
+N_CLUSTER=$(expr $N_CLUSTER - 1) # pythonic counting 
 
 for SET in $(seq 0 ${COUNT}); do
     for CLUSTER in $(seq 0 ${N_CLUSTER}); do
-        #CLUSTER=$(expr $N_CLUSTERS - $CLUSTER)
+        TMP=$(expr $N_CLUSTER - $CLUSTER)
         #SET=$(expr $N_SETS - $SET)
-        CLUSTER_PREFIX=$(printf "%02d" ${CLUSTER})
+        CLUSTER_PREFIX=$(printf "%02d" ${TMP})
         if [[ ! -f /net/so4/landclim/bverena/large_files/climfill_esa/${TESTCASE}/verification/set${SET}/clusters/datacluster_iter_c${CLUSTER_PREFIX}.nc ]]; then 
-            echo "set ${SET} cluster ${CLUSTER} calc ..."
-            python regression_learning.py -t ${TESTCASE} -s ${SET} -c ${CLUSTER}
+            echo "set ${SET} cluster ${CLUSTER_PREFIX} calc ..."
+            python regression_learning.py -t ${TESTCASE} -s ${SET} -c ${CLUSTER_PREFIX}
         else
             echo "set ${SET} cluster ${CLUSTER_PREFIX} already exists. skipping ..."
         fi
