6ef890b100b982410a723936e7f3545d0ea77c65
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2023-01-19 20:01:57 +0100

    not mask out, save crossval mask as wel

--- postprocessing_sets.py
+++ postprocessing_sets.py
@@ -18,20 +18,24 @@ esapath = '/net/so4/landclim/bverena/large_files/climfill_esa/'
 varnames = ['soil_moisture','surface_temperature','precipitation',
             'terrestrial_water_storage','temperature_obs','precipitation_obs',
             'snow_cover_fraction','diurnal_temperature_range','burned_area'] 
-label = 'interpolated'
-mask_orig = xr.open_dataset(f'{esapath}mask_orig.nc')
+label = 'climfilled'
+#mask_orig = xr.open_dataset(f'{esapath}mask_orig.nc')
 
 fill = xr.open_dataset(f'{esapath}{testcase}/verification/set{veriset}/data_{label}.nc')
 mask_cv = xr.open_dataset(f'{esapath}{testcase}/verification/set{veriset}/mask_crossval.nc')
 
-mask = np.logical_and(np.logical_not(mask_orig), mask_cv)
+#mask = np.logical_and(np.logical_not(mask_orig), mask_cv)
+mask = mask_cv
 fill = fill.to_array().reindex(variable=varnames)
 mask = mask.to_array().reindex(variable=varnames)
 
-fill = fill.where(mask)
+#fill = fill.where(mask)
 
 verification_year = slice('2004','2005')
 fill = fill.sel(time=verification_year)
+mask = mask.sel(time=verification_year)
 
 fill = fill.expand_dims(veriset=[veriset])
+mask = mask.expand_dims(veriset=[veriset])
 fill.to_dataset('variable').to_netcdf(f'{esapath}{testcase}/verification/set{veriset}/data_{label}_del.nc')
+mask.to_dataset('variable').to_netcdf(f'{esapath}{testcase}/verification/set{veriset}/mask_crossval_del.nc')
